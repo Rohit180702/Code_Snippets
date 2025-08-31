466a2745f245947bca813736b605fb88eb4d5574
Rohit1807 <rohit180702@users.noreply.github.com>
2025-08-31 22:50:45 +0530

    Binary Search

---
+++ C++/Binary_search/Binary_search.cpp
@@ -0,0 +1,15 @@
+    int binarySearch(vector<int> &nums, int target) {
+        int ans = -1;
+        int low = 0, high = nums.size()-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target)
+            {
+                return ans;
+            }
+            else if(nums[mid]>target) high = mid-1;
+            else low = mid + 1;
+        }
+        return ans;
+    }
---
+++ C++/Binary_search/lower_bound.cpp
@@ -0,0 +1,17 @@
+int lowerbound(vector<int>&nums, int target)
+    {
+        int ans = nums.size();
+        int low = 0, high = nums.size()-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid]>=target) {   //main logic
+                ans = mid;
+                high = mid-1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
